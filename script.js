2b7eac016b17ae5224015adb2e6c92443d917fdb
luobryan <bryanluo100@gmail.com>
2020-09-15 14:45:03 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function hi(){
+    document.getElementById('content').innerHTML = `<div class="container">
+    Hello! My name is <span style="color:#129cce">Bryan Luoo</span>
+    <br/> <br/>
+    <i class="fab fa-linkedin"></i>
+    <i class="fab fa-github"></i>
+    <i class="fas fa-envelope"></i>
+    <i class="fas fa-file"></i>
+    <br/>
+    </div>`;
+}
